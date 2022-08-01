d2a885d5d78af2e5dd28a4ce291bcebacf37d16a
Shivam Bhasin <bhasinshivam2002@gmail.com>
2022-08-01 18:25:20 +0530

    added feature to sort success streaks and add one milestone while challenge completion

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,12 +7,14 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  Tooltip,
 } from "@chakra-ui/react";
-import { FaPlus, FaCheck } from "react-icons/fa";
+import { FaPlus, FaCheck, FaFilter, FaInfo } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 
 function App() {
   const [show, setShow] = useState(false);
+  const [filterSuccess, setFilterSuccess] = useState(false);
   const [challenges, setChallenges] = useState(
     localStorage.getItem("challenges") === undefined
       ? []
@@ -24,6 +26,8 @@ function App() {
   }, [challenges, setChallenges]);
   const challengeName = useRef(null);
   const challengeDuration = useRef(null);
+  const milestoneMessage = useRef(null);
+  const checkpoint = useRef(null);
   return (
     <div className="min-h-screen w-screen bg-amber-400 overflow-hidden">
       <header className="flex flex-row bg-black/5 h-[60px] border-b-2 border-b-white/20 justify-center">
@@ -31,19 +35,40 @@ function App() {
           <span className="text-lg md:text-3xl font-bold">
             Gamified Streaks
           </span>
-          <FaPlus
-            className="px-1 border-4 border-black cursor-pointer hover:bg-black hover:text-white "
-            onClick={() => setShow((prevState) => !prevState)}
-            size="2rem"
-          />
+          <span className="flex gap-4">
+            <FaPlus
+              className="px-1 border-4 border-black cursor-pointer hover:bg-black hover:text-white "
+              onClick={() => setShow((prevState) => !prevState)}
+              size="2rem"
+            />
+            <FaFilter
+              className={`px-1 border-4 border-black cursor-pointer hover:bg-black hover:text-white ${
+                filterSuccess && "bg-black text-white "
+              }`}
+              onClick={() => setFilterSuccess((prevState) => !prevState)}
+              size="2rem"
+            />
+            <Tooltip
+              hasArrow
+              label="Create a challenge with the + icon, tap the + button in the challenge before 1.5 days to carry the streak"
+              bg="black"
+              color="white"
+            >
+              <FaInfo
+                className="p-1 border-4 border-black cursor-pointer hover:bg-black hover:text-white "
+                onClick={() => setFilterSuccess((prevState) => !prevState)}
+                size="2rem"
+              />
+            </Tooltip>
+          </span>
         </div>
       </header>
       <main className="flex flex-col w-[700px] mx-auto gap-2 p-4">
         {show && (
-          <section className="bg-transparent/20 p-4 rounded-xl border-black/80 border-4">
+          <section className="bg-transparent/20 p-4 rounded-xl border-black/80 border-4 flex flex-col gap-4">
             <div className="flex justify-between w-full items-center gap-4">
               <Input
-                placeholder="Basic usage"
+                placeholder="Challenge Name"
                 variant="flushed"
                 borderColor="black"
                 _placeholder={{ opacity: 0.5, color: "black" }}
@@ -75,6 +100,10 @@ function App() {
                             duration: parseInt(challengeDuration.current.value),
                             currentDay: Date.now(),
                             streak: 0,
+                            milestone: {
+                              checkpoint: parseInt(checkpoint.current.value),
+                              message: milestoneMessage.current.value,
+                            }
                           },
                         ]
                       : [
@@ -93,6 +122,28 @@ function App() {
                 size="2rem"
               />
             </div>
+            <div className="flex justify-between w-full items-center gap-4">
+              <NumberInput
+                size="md"
+                defaultValue={1}
+                min={1}
+                allowMouseWheel
+                borderColor="black"
+              >
+                <NumberInputField ref={checkpoint} />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <Input
+                placeholder="Milestone message"
+                variant="flushed"
+                borderColor="black"
+                _placeholder={{ opacity: 0.5, color: "black" }}
+                ref={milestoneMessage}
+              />
+            </div>
           </section>
         )}
         <article className="flex flex-col-reverse gap-4">
@@ -100,6 +151,7 @@ function App() {
             challenges.map((challenge, i) => {
               let failed = (Date.now() - challenge.currDay) / 86400000 > 1.5;
               let complete = challenge.streak >= challenge.duration;
+              if (filterSuccess && !complete) return null;
               return (
                 <section
                   key={i}
@@ -156,6 +208,9 @@ function App() {
                       }}
                     />
                   </div>
+                  {challenge.milestone.checkpoint <=challenge.streak &&  <div className="flex gap-4 items-center">
+                    <FaCheck /> {challenge.milestone.message}
+                  </div>}
                 </section>
               );
             })}
